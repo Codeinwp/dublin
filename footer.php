d644c3eccf1b89604a1e11cf09d9d1c4c5ecf8ef
abaicus <baicusandrei@me.com>
2015-12-15 16:24:34 +0200

    !!!Changed footer link.

--- footer.php
+++ footer.php
@@ -12,12 +12,12 @@
 	<?php if ( is_active_sidebar( 'sidebar-3' ) || is_active_sidebar( 'sidebar-4' ) || is_active_sidebar( 'sidebar-5' ) ) : ?>
 		<?php get_sidebar('footer'); ?>
 	<?php endif; ?>
-	
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'dublin' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'dublin' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'dublin' ), 'Dublin', '<a href="http://flyfreemedia.com/themes/dublin" rel="designer">Fly Free Media</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'dublin' ), 'Dublin', '<a href="http://justfreethemes.com/dublin" rel="nofollow">JustFreeThemes</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
