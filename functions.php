0818c06f55f32e120239a8db6c55cef874447530
Hardeep Asrani <me@hardeepasrani.com>
2017-06-03 22:01:52 +0530

    Enqueue html5shiv properly
    Enqueue html5shiv properly. Closes https://github.com/Codeinwp/dublin/issues/16

--- functions.php
+++ functions.php
@@ -170,6 +170,10 @@ function dublin_scripts() {
 
 	wp_enqueue_script( 'dublin-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/js/html5shiv.js' );
+
+	wp_script_add_data( 'html5shiv', 'conditional', 'IE' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -187,16 +191,6 @@ function dublin_excerpt_length( $length ) {
 
 }
 add_filter( 'excerpt_length', 'dublin_excerpt_length', 999 );
-
-/**
- * Load html5shiv
- */
-function dublin_html5shiv() {
-    echo '<!--[if lt IE 9]>' . "\n";
-    echo '<script src="' . esc_url( get_template_directory_uri() . '/js/html5shiv.js' ) . '"></script>' . "\n";
-    echo '<![endif]-->' . "\n";
-}
-add_action( 'wp_head', 'dublin_html5shiv' );
 
 /**
  * Row style for the page builder
